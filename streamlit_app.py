5aee2597f1fcb98b49501e9dec7481a9a9c2f89f
VetonFe <vetonferizaj4@gmail.com>
2022-12-23 18:13:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,34 @@
 import streamlit as st
 
-# Ky funksion merr si input nje fjali nga useri
-# dhe e bon ndarjen e fjalise ne fjale,
-# bon return nje liste me fjalet e fituara
-
 def convert_list(user_text):
+    '''
+    This function takes a text input from the user
+    it seperates the text in words, it returns
+    a list containing those words
+    '''
     text_list=user_text.split()
     return text_list
 
-# Ky funksion merr si input listen return te funksionit
-# convert_list dhe i shendrron fjalet ne upper case,
-# bon return nje liste me ato fjale
-
 def convert_upper(text_list):
+    '''
+    This function takes the return list of convert_list
+    function as input and converts all the words in upper case,
+    it returns a new list containing those words
+    '''
     new_text_list=[]
     for item in text_list:
         new_item=item.upper()
         new_text_list.append(new_item)
     return new_text_list
 
-# Ky funksion merr si input listen return te funksionit
-# convert_list dhe e bon return numrin e elementeve ne te
-
 def count(text_list):
+    '''
+    This function takes the return list of convert_list
+    function as input, it returns the number of elements in it
+    '''
     return len(text_list)
 
-# Ne user_text ruhet fjalia e userit
-
 user_text=st.text_input('Enter input: ')
-# Ne text_list ruhet returni i funksionit convert_list
 text_list=convert_list(user_text)
 
 if st.button('Return List'):
